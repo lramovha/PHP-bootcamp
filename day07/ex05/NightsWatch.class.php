4838b4f00f71ca6c37a34a8500ce05d0f7418488
lasykunte <lasyramovha53@gmail.com>
2019-10-17 18:08:46 +0200

    day07 projects

---
+++ day07/ex05/NightsWatch.class.php
@@ -0,0 +1,13 @@
+<?php
+	class NightsWatch implements IFighter {
+		private $_fight;
+		public function recruit ($meat) {
+			if ($meat instanceof IFighter) {
+				$_fight .= $meat->fight();
+			}
+		}
+		public function fight() {
+			print($_fight);
+		}
+	}
+?>
